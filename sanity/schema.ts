5e5f85017664fea80faeb16d81604dbae2bc5553
Guillermo Alfaro <galfarotolon@gmail.com>
2025-04-03 21:54:25 -0500

    fix erros

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,8 +2,6 @@ import { type SchemaTypeDefinition } from 'sanity'
 
 import blockContent from './schemaTypes/blockContent'
 import category from './schemaTypes/category'
-import post from './schemaTypes/post'
-import author from './schemaTypes/author'
 import homepage from './schemaTypes/homepage'
 import slide from './schemaTypes/slide'
 import navigation from './schemaTypes/navigation'
